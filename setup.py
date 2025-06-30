1169930ce16b98454b6d0e3f6caa65e09f448925
Jintao Zhang <59364182+jt-zhang@users.noreply.github.com>
2025-06-30 11:33:39 +0800

    add License

--- setup.py
+++ setup.py
@@ -1,15 +1,27 @@
+"""
+Copyright (c) 2024 by SageAttention team.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
 from setuptools import setup
 
 setup(
     name="sparse_sageattn",
     version="0.1.0",
-    packages=["sparse_sageattn"],  # 主包
-    package_dir={"sparse_sageattn": "sparse_sageattn"},  # 包目录位置
-    # 显式声明包内的模块（确保所有文件被包含）
+    packages=["sparse_sageattn"],  
+    package_dir={"sparse_sageattn": "sparse_sageattn"},  
     package_data={
-        "sparse_sageattn": ["*.py"],  # 包含所有.py文件
-        # 如果需要非Python文件（如.json/.so）：
-        # "sparse_sageatin": ["*.py", "data/*.json"],
+        "sparse_sageattn": ["*.py"],  # 
     },
-    # 其他配置...
 )
